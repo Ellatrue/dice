49c748f544e5e56f8e73f83ee637ea561bd6eee6
Ella Haines <ellatrue@yahoo.com>
2015-04-15 11:00:47 -0600

    Filled in use case for job seeker and "when & where" part of use case for employers.

--- specification/usecase.php
+++ specification/usecase.php
@@ -9,11 +9,30 @@
 		</h1>
 		<h2> Job Seekers</h2>
 		<h3> When and Where:</h3>
+			<ul>
+				<li>Evening</li>
+				<li>From home</li>
+				<li>Consistent data and power on desktop/laptop</li>
+				<li>Possible printer</li>
+			</ul>
 		<h3> Goal:</h3>
+			<p>To apply for a job by submitting an application.</p>
 		<h3> Steps:</h3>
+			<ul>
+				<li>Read job description and determine if the job is a good fit</li>
+				<li> If job is not a good fit, consider "Similar Positions" links in right panel</li>
+				<li>Possibly save or email job for later application</li>
+				<li>Hit "apply now" button to be redirected to employer's site and begin application process</li>
+			</ul>
 
 		<h2>Employers</h2>
 		<h3> When and Where:</h3>
+			<ul>
+				<li>During work day (8am-5pm)</li>
+				<li>From office</li>
+				<li>Consistent data and power on desktop/laptop</li>
+				<li>printer available</li>
+			</ul>
 		<h3> Goal:</h3>
 		<h3> Steps:</h3>
 
