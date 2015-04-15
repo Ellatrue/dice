f09e88fd37cc60618fc944443ba56f7381e7ab44
Ella Haines <ellatrue@yahoo.com>
2015-04-15 11:12:16 -0600

    Filled in the rest of the employer use case (goal, steps). Cleaned up appearance of code by adding/removing line breaks in html document.

--- specification/usecase.php
+++ specification/usecase.php
@@ -4,9 +4,8 @@
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-		<h1>
-			Use Cases for Dice.com Job Description
-		</h1>
+		<h1>Use Cases for Dice.com Job Description</h1>
+
 		<h2> Job Seekers</h2>
 		<h3> When and Where:</h3>
 			<ul>
@@ -34,8 +33,12 @@
 				<li>printer available</li>
 			</ul>
 		<h3> Goal:</h3>
+			<p>Have prospective employees sent from dice page to company's job application form.</p>
 		<h3> Steps:</h3>
-
+			<ul>
+				<li>Post job description and requirements on Dice</li>
+				<li>Check that job description page looks acceptable</li>
+				<li>Wait, hope for applications</li>
+			</ul>
 	</body>
-
 </html>
