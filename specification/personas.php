3c2187fd969567107eae26031b962c3af619a864
Ella Haines <ellatrue@yahoo.com>
2015-04-15 10:08:38 -0600

    Added personas and usecase pages with basic html structure.

---
+++ specification/personas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>
+			Personas for Dice.com Job Description
+		</h1>
+		<h2> Persona 1 </h2>
+		<p>
+
+		</p>
+		<h2>Persona 2</h2>
+		<p>
+
+		</p>
+
+	</body>
+</html>
---
+++ specification/usecase.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>
+			Use Cases for Dice.com Job Description
+		</h1>
+		<h2> Desktop Users</h2>
+		<p>
+
+		</p>
+		<h2>Mobile Users</h2>
+		<p>
+
+		</p>
+
+	</body>
+
+</html>
