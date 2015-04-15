0706ca79e3bf75e973249fd5a6c837bc153db803
Ella Haines <ellatrue@yahoo.com>
2015-04-15 10:16:56 -0600

    Filled in two personas, one for an employer and one for a job seeker.

--- specification/personas.php
+++ specification/personas.php
@@ -7,13 +7,20 @@
 		<h1>
 			Personas for Dice.com Job Description
 		</h1>
-		<h2> Persona 1 </h2>
+		<h2> Job Seeker </h2>
 		<p>
-
+			Age: 23-60 <br/>
+			Goals: To find a technical job that matches his/her skills and submit a resume and cover letter.<br/>
+			Device: Desktop Computer <br/>
+			Platform: Windows or Mac OS <br/>
 		</p>
-		<h2>Persona 2</h2>
+		<h2> Employer</h2>
 		<p>
-
+			Age: 35-60 <br/>
+			Goals: To post a job description and collect resumes and cover letters from prospective employees
+				for a technology job.<br/>
+			Device: Desktop Computer <br/>
+			Platform: Windows or Mac OS <br/>
 		</p>
 
 	</body>
